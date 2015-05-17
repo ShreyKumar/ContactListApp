1ecf9c45565863a5d02a4c3b9ef81e927f2cd899
Shreyansh Kumar <shreyansh.kumar@hotmail.com>
2015-05-17 17:53:41 -0400

    configured mongodb

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist', function(req, res){
     console.log("I recieved a GET request");
 
-     person1 = {
-         name: 'Tim',
-         email: 'tim@email.com',
-         number: '(111) 111-1111'
-     };
-     person2 = {
-         name: 'Timo',
-         email: 'timo@email.com',
-         number: '(114) 111-1111'
-     };
-     person3 = {
-         name: 'Time',
-         email: 'time@email.com',
-         number: '(111) 191-1111'
-     };
-     var contactlist = [person1, person2, person3];
-    res.json(contactlist);
+    db.contactlist.find(function(err, docs){
+        console.log(docs);
+        res.json(docs);
+    });
 });
     
 
