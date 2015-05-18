b9894f022713ad853d0a6e1b4f4c5914649f7d29
Shreyansh Kumar <shreyansh.kumar@hotmail.com>
2015-05-18 19:19:33 -0400

    added delete mechanism, fixing edit

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,11 +4,37 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
     
     console.log("Hello world from controller");
     
-    $http.get('/contactlist').success(function(response){
-        console.log("I got data I requested");
-        $scope.contactlist = response;
-    });
+
+    var refresh = function(){
+        $http.get('/contactlist').success(function(response){
+            console.log("I got data I requested");
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    }
+refresh();
     
+    $scope.addContact = function() {
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).success(function(response){
+          console.log(response);  
+        refresh();
+        });
+    };
+    
+    $scope.remove = function(id){
+        console.log(id);
+        $http.delete("/contactlist/" + id).success(function(response){
+            refresh();
+        });
+    }
+    
+    $scope.edit = function(id){
+        console.log(id);
+        $http.get('/contactlist' + id).success(function(response){
+           $scope.contact = response; 
+        });
+    };
 
     
 }]);
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res){
     console.log("I recieved a GET request");
@@ -14,6 +16,28 @@ app.get('/contactlist', function(req, res){
     });
 });
     
+app.post('/contactlist', function(req, res){
+    console.log(req.body);
+    db.contactlist.insert(req.body, function(err, doc){
+        res.json(doc);
+    });
+});
+
+app.delete("/contactlist/:id", function(req, res){
+    var id = req.params.id; 
+    console.log(id);
+    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    });
+});
+
+app.get('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    });
+});
 
 app.listen(3000);
 console.log("Server running");
