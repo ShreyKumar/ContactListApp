79d8efaf9dd9e0c40e0cbb1d1e50916b17f5d3bc
Shreyansh Kumar <shreyansh.kumar@hotmail.com>
2015-05-24 21:19:08 -0400

    fixed bug

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -6,7 +6,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
     
 
     var refresh = function(){
-        $http.get('/contactlist').success(function(response){
+        $http.get('/identity').success(function(response){
             console.log("I got data I requested");
             $scope.contactlist = response;
             $scope.contact = "";
@@ -16,7 +16,7 @@ refresh();
     
     $scope.addContact = function() {
         console.log($scope.contact);
-        $http.post('/contactlist', $scope.contact).success(function(response){
+        $http.post('/identity', $scope.contact).success(function(response){
           console.log(response);  
         refresh();
         });
@@ -24,21 +24,21 @@ refresh();
     
     $scope.remove = function(id){
         console.log(id);
-        $http.delete("/contactlist/" + id).success(function(response){
+        $http.delete("/identity/" + id).success(function(response){
             refresh();
         });
     }
     
 $scope.edit = function(id) {
   console.log(id);
-  $http.get('/contactlist/' + id).success(function(response) {
+  $http.get('/identity/' + id).success(function(response) {
     $scope.contact = response;
   });
 }; 
     
     $scope.update = function(){
         console.log($scope.contact._id);
-        $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+        $http.put('/identity/' + $scope.contact._id, $scope.contact).success(function(response){
             refresh();
         });
     }
@@ -46,6 +46,54 @@ $scope.edit = function(id) {
     $scope.deselect = function(){
         $scope.contact = "";
     };
+    
+    $scope.validate = function(){
+        try {
+            var user_len = $scope.signup_username.length;
+        } catch(TypeError){
+            var user_len = 0;
+        }
+        try {
+            var pass_len = $scope.signup_password.length;
+        } catch(TypeError){
+            var pass_len = 0;   
+        }
+        try {
+            var confirm_len = $scope.signup_confirmpwd.length;   
+        } catch(TypeError){
+            var confirm_len = 0;   
+        }
+        try {
+            var ans_len = $scope.ans.length;   
+        } catch(TypeError){
+            var ans_len = 0;
+        }
+        
+        var ready = true;
+        
+        if(user_len == 0 || pass_len == 0 || confirm_len == 0 || ans_len == 0){
+            $scope.signup_msg = "Some of your fields are empty. Please try again.";
+            ready = false;
+        };
+        
+        if($scope.signup_password != $scope.signup_confirmpwd) {
+            $scope.signup_msg = "Passwords do not match";
+            ready = false;
+        };
+        
+        if($scope.ans != "7"){
+            $scope.signup_msg = "Do you even math? The security question's answer is incorrect";
+            ready = false;
+        };
+        
+        return ready;
+    };
+    
+    $scope.signup = function(){
+        if($scope.validate()){
+            
+        }
+    }
 
     
 }]);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
+var db = mongojs('identity', ['identity']);
 var bodyParser = require('body-parser');
 //var passport = require('passport'), FacebookStrategy = require('passport-facebook').Strategy;
 
@@ -45,42 +45,42 @@ app.get('/auth/facebook/callback',
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-app.get('/contactlist', function(req, res){
+app.get('/identity', function(req, res){
     console.log("I recieved a GET request");
 
-    db.contactlist.find(function(err, docs){
+    db.identity.find(function(err, docs){
         console.log(docs);
         res.json(docs);
     });
 });
     
-app.post('/contactlist', function(req, res){
+app.post('/identity', function(req, res){
     console.log(req.body);
-    db.contactlist.insert(req.body, function(err, doc){
+    db.identity.insert(req.body, function(err, doc){
         res.json(doc);
     });
 });
 
-app.delete("/contactlist/:id", function(req, res){
+app.delete("/identity/:id", function(req, res){
     var id = req.params.id; 
     console.log(id);
-    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+    db.identity.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
         res.json(doc);
     });
 });
 
-app.get('/contactlist/:id', function(req, res){
+app.get('/identity/:id', function(req, res){
     var id = req.params.id;
     console.log(id);
-    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+    db.identity.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
         res.json(doc);
     });
 });
 
-app.put('/contactlist/:id', function(req, res){
+app.put('/identity/:id', function(req, res){
     var id = req.params.id;
     console.log(req.body.name);
-    db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+    db.identity.findAndModify({query: {_id: mongojs.ObjectId(id)},
     update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
     new: true}, function(err, doc){
         res.json(doc);
